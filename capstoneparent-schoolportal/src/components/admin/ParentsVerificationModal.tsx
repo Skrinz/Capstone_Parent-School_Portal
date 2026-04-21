28f9093e5b5e8e9d4b31cc61f5a24ef10a667eee
Skrinz <111281257+Skrinz@users.noreply.github.com>
2026-04-21 23:55:46 +0800

    Added timestamp and refresh button in manage parent verification

--- capstoneparent-schoolportal/src/components/admin/ParentsVerificationModal.tsx
+++ capstoneparent-schoolportal/src/components/admin/ParentsVerificationModal.tsx
@@ -52,6 +52,10 @@ const formatSubmittedAt = (value: string) => {
 		month: "long",
 		day: "numeric",
 		year: "numeric",
+		hour: "2-digit",
+		minute: "2-digit",
+		second: "2-digit",
+		hour12: true,
 	}).format(parsedDate);
 };
 
